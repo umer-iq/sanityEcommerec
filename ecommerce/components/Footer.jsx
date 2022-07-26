e3fa778d16476b3636fe3926770f20766de80da2
umer-iq <umeritsolutions@gmail.com>
2022-07-26 18:42:59 +0500

    insta

--- ecommerce/components/Footer.jsx
+++ ecommerce/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <p>2022 Store.PK  All rights reserved</p>
         <p className='icons'>
             
-           <a href='https://www.w3schools.com'><AiFillInstagram /></a>
+           <a href='https://www.instagram.com/umeriq/'><AiFillInstagram /></a>
             <AiOutlineTwitter/>
         </p>
     </div>
